b65242871f60da10579dd830a7aa3d57258da00a
ONEZWA22 <onezwaonezwa21@gmail.com>
2023-04-04 09:52:22 +0200

    modified code

--- Challenge1/Challenge1.js
+++ Challenge1/Challenge1.js
@@ -1,44 +1,45 @@
-WARNING = 'Free shipping only applies to single customer orders'
+FREE_WARNING = 'Free shipping only applies to single customer orders'
 BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
 NONE_SELECTED = '0'
-const RSA = 'RSA'
-const NAM = 'NAM'
+let RSA = 'South Africa'
+let NAM = 'Namibia'
+let currency = null
+let customers = '1'
 
-let shipping = 0
-let currency = '$'
 
 if (location === RSA) { 
-      shipping = 400  
-      currency = 'R' 
-} else if (location === NAM){
-      shipping = 600 
-      currency = currency
-     } 
-
-else {
-       shipping 
-       currency 
+      shipping === 400 && currency === 'R' 
+}
+if (location === NAM){     
+      shipping = 600
+} else {
+shipping = 800
 }
 
-const shoes = 300 * 1
-const toys = 100 * 5
-const shirts = 150 * 'NONE_SELECTED'
-const batteries = 35 * 2
-const pens = 5 * 'NONE_SELECTED' 
 
 
+let shoes = 300 * 1
+let toys = 100 * 5
+let shirts = 150 * 2
+let batteries = 35 * 2
+let pens = 5 * 20
 
-if (shoes + batteries + pens + shirts > 1000 ) {
-	if (location = NAM || RSA) {
-		    shipping = 0;
+shipping = null
+currency = 'R'
+
+if (shoes + batteries + pens + shirts > 1000     ) {
+	if (location = NAM && customers < 2) {
+			if (location === RSA)
+		    shipping = 0 || calcShipping
 		}
 	}
 
+if (shipping = 0 && customers !== 1) { 
+      console.log(WARNING) 
+}
 
-if (shipping = 0 && customers !== 1) { console.log(WARNING) }
+if (location === 'NK') {console.log(WARNING)} 
+console.log('Price:', currency, shoes + batteries + pens + shirts + shipping + toys)
 
-location = 'NAM' ? console.log(FREE_WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
-customers = '1'
 
-currency = null
